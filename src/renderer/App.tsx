8bb2f0e3db28fb0e8021a4b137f1c1da8547f63c
kun <Kunhcz@gmail.com>
2024-05-20 23:08:23 +0800

    demo

---
+++ src/renderer/App.tsx
@@ -0,0 +1,29 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
+      </div>
+      <h1>electron-demo</h1>
+      <div className="Hello">
+        <button type="button">检查更新</button>
+
+        <button type="button">开机自启动</button>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
